069840e36ece372a87594d6c3b223fbf1dbe8d85
Anali Balladares <analiballadares@gmail.com>
2022-07-01 11:48:44 -0500

    Fin seccion 7

--- src/app/pages/item/item.component.ts
+++ src/app/pages/item/item.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductosService } from '../../services/productos.service';
+import { ProductoDescripcion } from '../../interfaces/producto-descripcion.interface';
+import { Producto } from '../../interfaces/producto.interface';
 
 @Component({
   selector: 'app-item',
@@ -6,10 +10,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./item.component.css']
 })
 export class ItemComponent implements OnInit {
+  producto: ProductoDescripcion = {
+    categoria : '',
+    desc1 : '',
+    desc2 : '',
+    producto : '',
+    resumen : '',
+    subtitulo1 : '',
+    subtitulo2 : ''
 
-  constructor() { }
+  };
+  id: any;
+
+  constructor(private route : ActivatedRoute,
+                    public productoService: ProductosService   ) { }
 
   ngOnInit(): void {
+
+      this.route.params.subscribe(parametros => {
+       
+        
+        this.productoService.getProducto (parametros['id'])
+                .subscribe((producto  )=>{
+                  this.id =  parametros['id'];
+                  this.producto = producto;
+                 
+        });
+      });
+
   }
 
 }
