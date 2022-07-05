5dbb6daf15c366ace7be6e9fca318481bc23dd9d
Anali Balladares <analiballadares@gmail.com>
2022-07-05 12:27:48 -0500

    Actilizacion itemcomponent3

--- src/app/pages/item/item.component.ts
+++ src/app/pages/item/item.component.ts
@@ -21,11 +21,13 @@ export class ItemComponent implements OnInit {
 
     this.route.params
         .subscribe( parametros => {
-          // console.log(parametros['id']);
+           console.log(parametros['id']);
+           
           this.productoService.getProducto(parametros['id'])
                 .subscribe( (producto: ProductoDescripcion) => {
                   this.id = parametros['id'];
                   this.producto = producto;
+                  
                 });
         });
 
